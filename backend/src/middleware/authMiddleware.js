6fe8b548860eb7497b33ec3860ea67f892f50f99
maTworld-nerd <hakicha69@gmail.com>
2024-11-25 15:09:36 +0300

    ewe

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,10 @@
+// authMiddleware.js
+const isAuthenticated = (req, res, next) => {
+    if (req.session.user) {
+        next();
+    } else {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+};
+
+module.exports = isAuthenticated;
